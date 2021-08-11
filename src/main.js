a757c3990d29fbc6784d71ccfc55312bc87bcdc9
Rafael <rafacuevas3@gmail.com>
2021-08-11 11:45:25 -0500

    :hammer: (env) Move API KEY to env vars

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.use(VueSweetalert2)
 
-axios.defaults.headers.common['Authorization'] = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ'
+axios.defaults.headers.common['Authorization'] = process.env.VUE_APP_API_KEY
 
 Vue.use(VueAxios, axios)
 
